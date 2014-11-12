f20a83185b44d174292d232c0322e83e912101d9
hoangdt <hoangdt@smartosc.com>
2014-11-13 01:42:01 +0700

    4

--- js/vdownload.js
+++ js/vdownload.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
+	$("a.viewmore").click(function() {
+		$(this).hide();
+		$(this).prev('ul').css('max-height', 'none');
+		$(this).next('.viewless').show();
+	});
+	$("a.viewless").click(function() {
+		$(this).hide();
+		$(this).parent().children('ul').css('max-height', '403px');
+		$(this).prev('.viewmore').show();
+	});
 	$(".inline").colorbox({inline:true, width:"40%"});
 });
