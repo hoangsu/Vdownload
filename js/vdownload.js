378fd8947b3cbaf9ba5a075417d8e08833cf06d9
hoangdt <hoangdt@smartosc.com>
2014-11-14 01:34:24 +0700

    5

--- js/vdownload.js
+++ js/vdownload.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+	$('.home-slider').flexslider({
+		animation: "slide",
+		controlNav: "thumbnails",
+		directionNav:false
+	});
 	$("a.viewmore").click(function() {
 		$(this).hide();
 		$(this).prev('ul').css('max-height', 'none');
@@ -8,6 +13,15 @@ $(document).ready(function() {
 		$(this).hide();
 		$(this).parent().children('ul').css('max-height', '403px');
 		$(this).prev('.viewmore').show();
+	});
+	$("#closepop").click(function(event) {
+		$(this).parent().parent("#pop-app").hide('slow');
+	});
+	$('.pop-app-slider').flexslider({
+		animation: "slide",
+		controlNav: false,
+		slideshow: false
+
 	});
 	$(".inline").colorbox({inline:true, width:"40%"});
 });
