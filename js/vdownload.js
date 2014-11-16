dac4317fc1675b26aba3b7f50b6e0760bfb2ee4a
hoangdt <hoangdt@smartosc.com>
2014-11-16 21:16:16 +0700

    weeken

--- js/vdownload.js
+++ js/vdownload.js
@@ -4,16 +4,11 @@ $(document).ready(function() {
 		controlNav: "thumbnails",
 		directionNav:false
 	});
-	$("a.viewmore").click(function() {
-		$(this).hide();
-		$(this).prev('ul').css('max-height', 'none');
-		$(this).next('.viewless').show();
-	});
-	$("a.viewless").click(function() {
-		$(this).hide();
-		$(this).parent().children('ul').css('max-height', '403px');
-		$(this).prev('.viewmore').show();
-	});
+	viewMoreText(".block-top","403px");
+	viewMoreText(".description","120px");
+	slider4(".description",162,10);
+	slider4(".related-app",138,53);
+	slider4(".related-com",138,53);
 	$("#closepop").click(function(event) {
 		$(this).parent().parent("#pop-app").hide('slow');
 	});
@@ -23,5 +18,34 @@ $(document).ready(function() {
 		slideshow: false
 
 	});
+	$('.tabs li').click(function(){
+		$('.tabs li').removeClass('active');
+		$('.tab-content').removeClass('active');
+		$(this).addClass('active');
+		$($(this).find('a').attr("href") + '-content').addClass('active');
+	 });
 	$(".inline").colorbox({inline:true, width:"auto"});
 });
+function viewMoreText(Parent,mHeight){
+	$(Parent).find("a.viewmore").click(function() {
+		$(this).hide();
+		$(this).prev().css('max-height', 'none');
+		$(this).next('.viewless').show();
+	});
+	$(Parent).find("a.viewless").click(function() {
+		$(this).hide();
+		$(this).prev().prev().css('max-height', mHeight);
+		$(this).prev('.viewmore').show();
+	});
+}
+function slider4(parent,width,margin) {
+	$(parent+" .slider4").flexslider({
+		animation: "slide",
+	    itemWidth: width,
+	    itemMargin: margin,
+	    controlNav:false,
+	    minItems: 4,
+	    maxItems: 4,
+	    slideshow: false
+	})
+}
